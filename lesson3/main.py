26a3bc5b6e063acd3318077015a06e58ab3d1294
hari krisha <hari.k.krishna4@gmail.com>
2017-04-20 15:30:19 -0500

    added a Class Handler for eliminating the self.response.out.write to self.write

--- lesson3/main.py
+++ lesson3/main.py
@@ -2,10 +2,16 @@
 import webapp2
 
 
-class MainHandler(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+
+class MainHandler(Handler):
 
     def get(self):
-        self.response.write('just for the heck. why not?')
+        self.write('just for the heck. why not?')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
