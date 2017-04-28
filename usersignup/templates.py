ac3dbc60b826b72170fb13e4d7683f1fed3e3404
hari krisha <hari.k.krishna4@gmail.com>
2017-04-28 14:27:41 -0500

    not working but only 2 errors

--- usersignup/templates.py
+++ usersignup/templates.py
@@ -24,8 +24,47 @@ class Handler(webapp2.RequestHandler):
 
 class SignUp(Handler):
 
+    def valid_username(self, username):
+        USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+        return username and USER_RE.match(username)
+
+    def valid_password(self, password):
+        PASS_RE = re.compile(r"^,{3,20}$")
+        return password and PASS_RE.match(password)
+
+    def valid_email(self, email):
+        EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+        return not email or EMAIL_RE.match(email)
+
     def get(self):
         self.render("signup.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get('username')
+        password = self.request.get('passwords')
+        verify = self.request.get('verify')
+        email = self.request.get('email')
+
+        params = dict(username=username,
+                      email=email)
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+
+        if not valid_email(email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('signup.html', **params)
+        else:
+            self.redirect('/welcome.html' username)
 
 
 class WelcomePage(Handler):
@@ -33,6 +72,5 @@ class WelcomePage(Handler):
         self.render("welcome.html")
 
 
-
 app = webapp2.WSGIApplication(
-    [('/', SignUp),('/welcome', WelcomePage)], debug=True)
+    [('/', SignUp), ('/welcome', WelcomePage)], debug=True)
